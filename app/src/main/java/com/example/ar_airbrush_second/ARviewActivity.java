bed8057c3bf0173f518b2c3aea829c43f2a7b0aa
OllieHanton <60584823+OllieHanton@users.noreply.github.com>
2022-06-21 14:26:10 +0100

    changed making mode to create, settings no longer floating, toggle button more effective, added top text - O

--- app/src/main/java/com/example/ar_airbrush_second/ARviewActivity.java
+++ app/src/main/java/com/example/ar_airbrush_second/ARviewActivity.java
@@ -7,12 +7,15 @@ import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.AlertDialog;
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 //import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -42,14 +45,18 @@ public class ARviewActivity extends AppCompatActivity{
     //Design chosen from design library flag
     private int designChosen = 1;
 
-    //Design mode (0) or making mode (1) toggled flag
-    private int modeChosen;
+    //Design mode (0) or create mode (1) toggled flag
+    private int toggleMode=0;
 
-    //Novice level (with text) or expert level (no text) chosen for makeingmode... toggled flag
+    //Novice level (with text) or expert level (no text) - scale chosen for makeingmode... toggled flag. Maybe at lowest level have it so that
     private int novicelevel;
 
     //layout token for AR layout for dynamic layout implementation - taken from AR fragment
     private ArSceneView arSceneView;
+
+    private int sliderChangedValue;
+
+    Toast toastMessage;
 
     public static boolean checkSystemSupport(Activity activity) {
         // checking whether the API version of the running Android >= 24
@@ -78,64 +85,115 @@ public class ARviewActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_arview);
 
-            if (checkSystemSupport(this)) {
+        if (checkSystemSupport(this)) {
 
-                setUpAnchorPoints();
+            //Initiate ARCore functionality:
+            setUpAnchorPoints();
 
-
-                //work in progress for button
-                /*arSceneView=arCam.getArSceneView();
-                ArFragment arfragmentofscene = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ar_camera_area);
-                View arView = arfragmentofscene.getArSceneView();
-                ConstraintLayout Arlayout = (ConstraintLayout) arView.findViewById(R.id.ar_camera_area);
-                Button testbutton = new Button(this);
-                Arlayout.addView(testbutton);*/
-
-
-                FloatingActionButton fab = findViewById(R.id.down_button);
-                fab.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-
+            //initiate objects relating to each overlay button
+            FloatingActionButton togglebutton = findViewById(R.id.toggle_mode_button);
+            ImageButton settingsdropdown = findViewById(R.id.settings_drop_down_menu);
+            TextView costevaluator = findViewById(R.id.designmode_text_cost_evaluator);
+            TextView substratedistance = findViewById(R.id.createmode_text_distance_to_substrate);
+            SeekBar slider = findViewById(R.id.createmode_seekbar);
+            FloatingActionButton backbutton = findViewById(R.id.createmode_back);
+            FloatingActionButton forwardsbutton = findViewById(R.id.createmode_forward);
+            TextView topTextInstructions = findViewById(R.id.createmode_top_text_instructions);
+            //handle slider functionality - script here?
+            slider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int scriptProgress, boolean fromUser) {
+                    sliderChangedValue = scriptProgress;
+                    if (toastMessage!= null) {
+                        toastMessage.cancel();
                     }
-                });
-
-    /*private View myView;
-    FloatingActionButton myFab = (FloatingActionButton) myView.findViewById(R.id.down_button);
-    myFab.setOnClickListener(new View.OnClickListener() {
-        public void onClick(View v) {
-            doMyThing();
-        }
-    });
-    FloatingActionButton deleteButton = findViewById(R.id.blank_button_bottom_right);
-        deleteButton.setOnClickListener(new View.OnClickListener() {
-        @Override*/
-
-        /*public void onClick(View view) {
-            //Delete the Anchor if it exists
-            Log.d(TAG,"Deleteing anchor");
-            int currentAnchorIndex;
-            if (numberOfAnchors < 1 ) {
-                Toast.makeText(LineViewMainActivity.this, "All nodes deleted", Toast.LENGTH_SHORT).show();
-                return;
-            }
-            removeAnchorNode(currentSelectedAnchorNode);
-            currentSelectedAnchorNode = null;
-
-            //Remove the line if it exists also
-            removeLine(nodeForLine);
-        }
-    });*/
-
-
-                if(modeChosen==0) {
-                    designMode();
+                    toastMessage=Toast.makeText(ARviewActivity.this, "Spraying phase " + sliderChangedValue, Toast.LENGTH_SHORT);
+                    toastMessage.show();
                 }
-                else{
-                    makingMode();
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
                 }
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+                }
+            });
 
+            //handle distance from substrate text:
+            //substratedistance.setTextColor(Color.WHITE);
+
+            //handle createmode back button
+            backbutton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int progressIncrementor=slider.getProgress();
+                    progressIncrementor--;
+                    if(progressIncrementor>=0 && progressIncrementor<=slider.getMax()) {
+                        slider.setProgress(progressIncrementor);
+                    }
+                }
+            });
+
+            //handle createmode forwards button
+            forwardsbutton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int progressIncrementor=slider.getProgress();
+                    progressIncrementor++;
+                    if(progressIncrementor>=0 && progressIncrementor<=slider.getMax()) {
+                        slider.setProgress(progressIncrementor);
+                    }
+                }
+            });
+
+            //Toggle between design and create modes
+            togglebutton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(toggleMode==0) {
+                        //set create mode
+                        costevaluator.setVisibility(View.INVISIBLE);
+                        substratedistance.setVisibility(View.VISIBLE);
+                        slider.setVisibility(View.VISIBLE);
+                        backbutton.setVisibility(View.VISIBLE);
+                        forwardsbutton.setVisibility(View.VISIBLE);
+                        toggleMode=1;
+                        togglebutton.setImageResource(R.drawable.airbrush_64);
+                        topTextInstructions.setText("You are now in Create Mode! Tap the toggle button to the left to return to Design Mode.");
+                    }
+                    else {
+                        //set design mode
+                        costevaluator.setVisibility(View.VISIBLE);
+                        substratedistance.setVisibility(View.INVISIBLE);
+                        slider.setVisibility(View.INVISIBLE);
+                        backbutton.setVisibility(View.INVISIBLE);
+                        forwardsbutton.setVisibility(View.INVISIBLE);
+                        toggleMode=0;
+                        togglebutton.setImageResource(R.drawable.design_icon);
+                        topTextInstructions.setText("You are in Design Mode! Slowly move camera to initiate augmented reality. When white dots appear, tap to place and edit your design.");
+                    }
+                }
+            });
+
+            //handle settings button in both design and create modes
+            settingsdropdown.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (toastMessage!= null) {
+                        toastMessage.cancel();
+                    }
+                    toastMessage=Toast.makeText(ARviewActivity.this, "Settings open", Toast.LENGTH_SHORT);
+                    toastMessage.show();
+                }
+            });
+
+            if(toggleMode==0) {
+                designMode();
             }
+            else {
+                designMode();
+                //createMode();
+            }
+        }
     }
 
     //initialise AR environment before entering design mode
@@ -158,6 +216,11 @@ public class ARviewActivity extends AppCompatActivity{
             if (clickNo == 1) {
 
                 Anchor anchor = hitResult.createAnchor();
+
+                //make costevaluator visible.. - this is currently buggy where it doesn't appear straight away.
+                TextView costevaluator = findViewById(R.id.designmode_text_cost_evaluator);
+                costevaluator.setVisibility(View.VISIBLE);
+
                 ModelRenderable.builder()
                         //Currently hardcoded text object:
                         .setSource(this, R.raw.dispray_texttiny)
@@ -170,18 +233,15 @@ public class ARviewActivity extends AppCompatActivity{
                             return null;
                         });
             }
-            //TextView textView = new TextView(this);
-            //textView.setText("BeepBoop");
-            //arSceneView.addView(textView);
         });
     }
 
     //alternative mode where object is fixed/editting no longer enabled and script/progress bar/forwardsback buttons are implemented
-    private void makingMode() {
+    private void createMode() {
         novicelevel=1;
     }
 
-    private void toggleMode() {
+    private void toggleExperience() {
         /*if(novicelevel==0){
             novicelevel=1;
         }
